f192a656d9c7ca70ba4085d804ae7df93f2a5d51
Dapo Oremade <83841353+dapsino007@users.noreply.github.com>
2021-05-08 18:51:23 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//Declare sound variables
 var crash = new Audio("sounds/crash.mp3");
 var kickBass = new Audio("sounds/kick-bass.mp3");
 var snare = new Audio("sounds/snare.mp3");
@@ -6,29 +7,16 @@ var tom2 = new Audio("sounds/tom-2.mp3");
 var tom3 = new Audio("sounds/tom-3.mp3");
 var tom4 = new Audio("sounds/tom-4.mp3");
 
-var selectButton = document.querySelectorAll(".drum");
-
 // Drum listening for clicks
-for(i=0; i<selectButton.length; i++){
-  selectButton[i].addEventListener("click", function (){
-    playSound(this.innerHTML);
-    animateDrum(this.innerHTML);
-  })
-}
+$("button").click(function(){playSound(this.innerHTML); animateDrum(this.innerHTML);});
 
 //Drum listening to keyboard
-document.addEventListener("keypress", function(event){
-  playSound(event.key);
-  animateDrum(event.key);
-})
+("body").keypress(function(event){playSound(event.key); animateDrum(event.key);})
 
-//add and time out class .pressed"
+//add and time out class ".pressed"
 function animateDrum(letterInput){
-  document.querySelector("." + letterInput).classList.add("pressed");
-  setTimeout(function () {
-    document.querySelector("." + letterInput).classList.remove("pressed");
-  }, 100);
-}
+  $("." + letterInput).addClass("pressed");
+  setTimeout(function () {$("." + letterInput).removeClass("pressed");}, 100);}
 
 // Function linking buttons to drum sounds
 function playSound(letter){
